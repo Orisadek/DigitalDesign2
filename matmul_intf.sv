c1157e6ae444b70916fb6f59adaeaf0702afa68d
Alon Vain <Alonix147@gmail.com>
2024-03-06 13:16:05 +0200

    update Golden
    update matmul_golden

--- matmul_intf.sv
+++ matmul_intf.sv
@@ -13,6 +13,6 @@ interface matmul_intf(input logic clk_i, input logic rst_ni);
 	modport DUT  (input clk_i,rst_ni,psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,output pready_o,pslverr_o,prdata_o,busy_o);
 	modport STIMULUS(output psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,input clk_i,rst_ni,pready_o,pslverr_o,prdata_o,busy_o);
 	modport CHECKCOV(input  clk_i,rst_ni,psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,output pready_o,pslverr_o,prdata_o,busy_o);
-  
+	modport GOLDEN(output psel_i,penable_i,pwrite_i,pstrb_i,pwdata_i,paddr_i,input clk_i,rst_ni,pready_o,pslverr_o,prdata_o,busy_o);
 endinterface
 
