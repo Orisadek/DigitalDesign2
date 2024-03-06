581a5dd5710aa3a762994738aa2b6fa432a3a5f5
Alon Vain <Alonix147@gmail.com>
2024-03-06 13:26:13 +0200

    addint matlab files and txt files
    add Matlab.m file as well as matA matB matC txt files

---
+++ matmul_golden/golden_model.m
@@ -0,0 +1,59 @@
+clc
+clear all
+
+% Define the file name
+MatrixA_file_name = 'MatrixA.txt';   %'MatrixA file name'
+MatrixB_file_name = 'MatrixB.txt';   %'MatrixB file name'
+MatrixC_file_name = 'MatrixC.txt';   %'MatrixC file name'
+% Open the file for writing
+
+fidA = fopen(MatrixA_file_name, 'w');
+fidB = fopen(MatrixB_file_name, 'w');
+fidC = fopen(MatrixC_file_name, 'w');
+
+%parameters
+
+num_matrices = 3;
+Maxnumber = 2^16;
+Minnumber = 0;
+
+for i = 1:num_matrices
+    % Generate matrices size
+    N = randi([1,4]);
+    K = randi([1,4]);
+    M = randi([1,4]);
+    % Generate random matrices
+    random_matrix_A = randi([Minnumber, Maxnumber], N, K);
+    random_matrix_B = randi([Minnumber, Maxnumber], K, M);
+    random_matrix_C = random_matrix_A * random_matrix_B;
+    
+    % Write the matrices to the files
+    % Save matrix A to file
+    fprintf(fidA, '%d x %d\n', N, K);
+    for row = 1:N
+        fprintf(fidA, '%f\t', random_matrix_A(row, :));
+        fprintf(fidA, '\n');
+    end
+    fprintf(fidA, '\n\n');
+
+    % Save matrix B to file
+    fprintf(fidB, '%d x %d\n', K, M);
+    for row = 1:K
+        fprintf(fidB, '%f\t', random_matrix_B(row, :));
+        fprintf(fidB, '\n');
+    end
+    fprintf(fidB, '\n\n');
+
+    % Save matrix C to file
+    fprintf(fidC, '%d x %d\n', N, M);
+    for row = 1:N
+        fprintf(fidC, '%f\t', random_matrix_C(row, :));
+        fprintf(fidC, '\n');
+    end
+    fprintf(fidC, '\n\n');
+end
+% Close the file
+fclose(fidA);
+fclose(fidB);
+fclose(fidC);
+disp(['Matrices saved to file: ', MatrixA_file_name, ', ', MatrixB_file_name, ', ', MatrixC_file_name]);
